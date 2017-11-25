1b499fa2b48378c4105689020111f0903adf2efa
vlnk <vlnk@protonmail.ch>
2017-11-25 17:39:28 -0500

    add create-react-app setup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './components/App'
-import reducer from './reducers'
-import 'antd/dist/antd.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer)
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
