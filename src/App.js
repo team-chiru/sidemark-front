f5e29e400d3fc4b926c72dafd871ca9dfde49fde
vlnk <vlnk@protonmail.com>
2017-11-26 14:21:15 -0500

    connect old component with create-react-app bundle

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+
+import { Layout } from 'antd'
+
+import EditMenu from './containers/EditMenu'
+import LikemarkTreeView from './containers/LikemarkTreeView'
+
+import logo from './logo.svg'
+import './App.css'
+import 'antd/dist/antd.css'
+
+const store = createStore(reducer)
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Provider className='App' store={store}>
+        <Layout>
+          <Layout.Header>
+            <img src={logo} className='App-logo' alt='logo' />
+            <EditMenu />
+          </Layout.Header>
+          <Layout.Content>
+            <LikemarkTreeView />
+          </Layout.Content>
+        </Layout>
+      </Provider>
+    )
   }
 }
 
-export default App;
+export default App
