444ad12dcd9192ed2d98399deb38a47ca74240e1
Jaelson Carvalho <jaelsoncarvalhojr@gmail.com>
2017-12-12 00:09:37 -0500

    Update js entry file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
+// Dependencies
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import axios from 'axios'
+import axiosMiddleware from 'redux-axios-middleware'
+import mainReducer from './ducks/index'
+import { getFirstChildren } from './ducks/children'
 
-import './index.css'
-import App from './App'
+// Root
+import Root from './root/root.component'
 
+// Config
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// Assets
+import './App.css'
+import './index.css'
+import 'antd/dist/antd.css'
+
+const client = axios.create({
+  baseURL: 'http://localhost:42506/likemark',
+  responseType: 'json'
+})
+
+const store = createStore(
+  mainReducer,
+  applyMiddleware(
+    axiosMiddleware(client)
+  )
+)
+
+store.dispatch(getFirstChildren(0))
+
+ReactDOM.render(<Root store={store} />, document.getElementById('root'))
 registerServiceWorker()
