602fb3b57b3a7deb3b2cd9b56ccbacceb939a36d
Jaelson Carvalho <jaelsoncarvalhojr@gmail.com>
2017-12-23 11:47:18 -0500

    Update general layout

--- src/pages/home/home.component.js
+++ src/pages/home/home.component.js
@@ -4,6 +4,9 @@ import React from 'react'
 // Components
 import Likemark from '../../containers/likemark/likemark.container'
 
+// Assets
+import './home.scss'
+
 // Main Component
 const Home = () => (
   <div id='home-page'>
