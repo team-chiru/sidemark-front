b860861bb55eb40bd6950f8aa37758c9864f7565
Jaelson Carvalho <jaelsoncarvalhojr@gmail.com>
2017-12-28 22:18:58 -0500

    Create LikemarkChild container

---
+++ src/containers/likemark-child/likemark-child.container.js
@@ -0,0 +1,42 @@
+/* @flow */
+
+// Dependencies
+import * as React from 'react'
+
+// Components
+import LikemarkChildUI from 'components/likemark-child/likemark-child.component'
+
+/**
+ * Display a Likemark Child component.
+ * @property {Function} props.handleLikemarkClick - Updates the likemark state.
+ * @property {Object} props.likemarkChild - The name of the children.
+ */
+type Props = {
+  handleLikemarkClick: (likemarkId: number) => void,
+  likemarkChild: {id: number, name: string, parentId: number, url: string}
+}
+
+type State = {
+  expandedDetails: boolean
+}
+
+export class LikemarkChild extends React.Component<Props, State> {
+  constructor (props: Props, context: any) {
+    super(props, context)
+    this.state = {
+      expandedDetails: false
+    }
+  }
+
+  render () {
+    return (
+      <LikemarkChildUI {...{
+        ...this.props,
+        toggleDetailsVisibilty: () => this.setState({expandedDetails: !this.state.expandedDetails}),
+        expandedDetails: this.state.expandedDetails
+      }} />
+    )
+  }
+}
+
+export default LikemarkChild
